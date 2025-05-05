233282cbbb556ebefea34570df9039d9643df2c2
lixel_ <133605174+lixelv@users.noreply.github.com>
2025-05-05 18:49:00 +0300

    Added startup command

--- startup.py
+++ startup.py
@@ -1,24 +1,19 @@
-computers = [{"mac": "68:54:5A:D3:F0:30"}, {"mac": "8C:C6:81:42:00:0F"}, {"mac": "E8:48:B8:C8:20:00"}]
+# test data
+computers = [
+    {"mac": "68:54:5A:D3:F0:30"},  # ...
+    {"mac": "8C:C6:81:42:00:0F"},  # ...
+    {"mac": "E8:48:B8:C8:20:00"},  # ...
+]
 
-import socket
-import re
+# comment computers adds variable computers,
+# that includes all computers,
+# witch were selected on execution moment
 
-def send_wol(mac_address):
-    if not re.match(r"^[0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5}$", mac_address):
-        raise ValueError(
-            "Неверный формат MAC-адреса. Используйте формат AA:BB:CC:DD:EE:FF."
-        )
+# COMPUTERS
 
-    mac_bytes = bytes.fromhex(mac_address.replace(":", ""))
-    magic_packet = b"\xff" * 6 + mac_bytes * 16
+from wakeonlan import send_magic_packet
 
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.sendto(magic_packet, ("255.255.255.255", 9))
-
-for computer in computers:
-    try:
-        send_wol(computer["mac"])
-        print(f"Wake-on-LAN пакет отправлен на ({computer['mac']}).")
-    except ValueError as e:
-        print(f"Ошибка: {e}")
+for pc in computers:
+    if pc["status"] == "0":
+        send_magic_packet(pc["mac"])
+        print(f"Sent magic packet to {pc['name']}")
