64808a50609119b443c8b18881dbf1472a5fd107
lixel_ <133605174+lixelv@users.noreply.github.com>
2025-05-07 18:08:41 +0300

    Using psutil to kill process

--- process_kill.py
+++ process_kill.py
@@ -1,4 +1,15 @@
-import os
+import psutil
 
-process_name = "{{PROCESS_NAME}}"
-os.system(f"taskkill /F /IM {process_name}")
+fragment = r"Docker"
+
+for proc in psutil.process_iter(['pid', 'name']):
+    try:
+        process_info = proc.info
+        process_name = process_info['name']
+        
+        if fragment in process_name:
+            proc.kill()
+            print(f"Killed process: PID={process_info['pid']}, Name={process_name}")
+    
+    except psutil.NoSuchProcess:
+        pass
